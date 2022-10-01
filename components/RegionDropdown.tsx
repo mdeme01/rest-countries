76287220bc1887198b3afcbf67bb58def20d7924
mdeme01 <se4n01@gmail.com>
2022-10-01 17:43:13 +0200

    refactor(): dropdown styling

--- components/RegionDropdown.tsx
+++ components/RegionDropdown.tsx
@@ -1,6 +1,9 @@
 import { useRouter } from 'next/router';
-import { styled } from '@mui/material';
+import { Button, styled } from '@mui/material';
 import { useState } from 'react';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+import { blue } from '@mui/material/colors';
 
 const Dropdown = styled('div')(({ theme }) => ({
   position: 'relative',
@@ -16,27 +19,31 @@ const Dropdown = styled('div')(({ theme }) => ({
 const DropdownContent = styled('div')(({ theme }) => ({
   backgroundColor: theme.backgroundColor.main,
   position: 'absolute',
-  minWidth: '160px',
+  width: '100%',
   boxShadow: '0px 8px 16px 0px rgba(0, 0, 0, 0.2)',
   padding: '12px 16px',
   zIndex: '1',
   gridTemplateRows: '1fr',
   gap: '1rem',
   borderRadius: '5px',
-  marginTop: '30px',
+  marginTop: '10px',
   cursor: 'pointer',
 }));
 
-const DropdownText = styled('span')(({ theme }) => ({
+const DropdownText = styled(Button)(({ theme }) => ({
   backgroundColor: theme.backgroundColor.main,
-  padding: '20px',
+  padding: '15px 30px',
   textAlign: 'center',
   borderRadius: '5px',
+  textTransform: 'none',
 }));
 
-const DropdownItem = styled('div')({
+const DropdownItem = styled('div')(({ theme }) => ({
   placeSelf: 'start',
-});
+  ':hover': {
+    color: theme.textColor.hover,
+  },
+}));
 
 export default function RegionDropdown() {
   const router = useRouter();
@@ -57,11 +64,13 @@ export default function RegionDropdown() {
   };
 
   return (
-    <Dropdown
-      onMouseEnter={() => setDropdownVisible(true)}
-      onMouseLeave={() => setDropdownVisible(false)}
-    >
-      <DropdownText>Filter by region: {region}</DropdownText>
+    <Dropdown>
+      <DropdownText
+        endIcon={dropdownVisible ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+        onClick={() => setDropdownVisible(!dropdownVisible)}
+      >
+        Filter by region: {region}
+      </DropdownText>
       <DropdownContent
         style={{
           display: dropdownVisible ? 'grid' : 'none',
